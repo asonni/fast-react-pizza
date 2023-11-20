91173a9258b1509156664a06c1270520768c1cb6
Aladdin Sonni <aladdin.sonni@gmail.com>
2023-11-20 20:11:21 +0200

    React Router With Data Loading

---
+++ src/features/menu/Menu.jsx
@@ -0,0 +1,25 @@
+/* eslint-disable react-refresh/only-export-components */
+import { useLoaderData } from 'react-router-dom';
+
+import MenuItem from '../menu/MenuItem';
+import { getMenu } from '../../services/apiRestaurant';
+
+function Menu() {
+  const menu = useLoaderData();
+  console.log(menu);
+
+  return (
+    <ul>
+      {menu.map(pizza => (
+        <MenuItem key={pizza.id} pizza={pizza} />
+      ))}
+    </ul>
+  );
+}
+
+export async function loader() {
+  const menu = await getMenu();
+  return menu;
+}
+
+export default Menu;
